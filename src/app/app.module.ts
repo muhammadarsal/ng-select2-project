5c3570aa8795515b3a8febfd6faa37f2ef300087
Muhammad Arsal Abdullah <arsal@plerosys.com>
2017-08-22 14:04:59 +0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgSelect2Module } from './ng-select2/index';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+		NgSelect2Module
   ],
   providers: [],
   bootstrap: [AppComponent]
